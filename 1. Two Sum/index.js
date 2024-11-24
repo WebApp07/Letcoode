85c8a39541366304f2beab26ac54b802d211f73f
unknown <aminezouguig@proton.me>
2024-11-24 02:04:35 +0100

    5. Majority Element

---
+++ 1. Two Sum/index.js
@@ -0,0 +1,9 @@
+var twoSum = function () {
+  let nums = [1, 2, 5, 7];
+  nums.forEach(function (num) {
+    console.log(num);
+    if (num[0] + nums[1])
+  });
+};
+
+twoSum();
---
+++ 2. Merge Sorted Array/index.js
@@ -0,0 +1,19 @@
+var merge = function (nums1, m, nums2, n) {
+  const len = m + n;
+  nums1.splice(len - n);
+  for (let i = 0; i < n; i++) {
+    nums1.push(nums2[i]);
+  }
+
+  for (let j = 1; j < len; j++) {
+    if (nums1[j - 1] || nums1[j - 1] === 0) {
+      if (nums1[j] < nums1[j - 1]) {
+        let tmp = nums1[j - 1];
+        nums1[j - 1] = nums1[j];
+        nums1[j] = tmp;
+        j = j - 2;
+      }
+    }
+  }
+  return nums1;
+};
