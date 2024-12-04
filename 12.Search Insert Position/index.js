b881a11fe7251fd28610f9fe7772bb07391aba6c
unknown <aminezouguig@proton.me>
2024-12-04 23:49:25 +0100

    Search insert position

---
+++ 12.Search Insert Position/index.js
@@ -0,0 +1,12 @@
+var searchInsert = function (nums, target) {
+  let [l, r] = [0, nums.length];
+  while (l < r) {
+    const mid = (l + r) >> 1;
+    if (nums[mid] >= target) {
+      r = mid;
+    } else {
+      l = mid + 1;
+    }
+  }
+  return l;
+};
